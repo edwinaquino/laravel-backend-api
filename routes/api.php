afe096e0799a74dd9eed4be84c3f3b89f1e3d27c
Edwin Aquino <github@edwinaquino.com>
2021-02-12 23:17:16 -0800

    API WORKING LOCALHOST

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CEOController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+# OLD WAY
+//Route::post('/register', 'API/AuthController@register');
+//Route::post('/login', 'API/AuthController@login');
+//Route::apiResource('/ceo', 'API/CEOController')->middleware('auth:api');
+
+# NEW WAY LARAVEL 8
+//https://stackoverflow.com/questions/63807930/target-class-controller-does-not-exist-laravel-8
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
+Route::apiResource('/ceo', 'App\Http\Controllers\API\CEOController')->middleware('auth:api');
